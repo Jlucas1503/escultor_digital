75cf7d93e07b80715140a6c295e99420bdf7ab4b
José Lucas da Silva Gomes <121811216+Jlucas1503@users.noreply.github.com>
2024-12-30 23:50:23 -0300

    adição da matriz 3d no construtor
    adicionei a formação da matriz 3d que vai servir de escopo para a escultura.

--- sculptor.cpp
+++ sculptor.cpp
@@ -7,9 +7,40 @@ Sculptor::Sculptor(int _nx, int _ny, int _nz){
     _ny = ny;
     _nz = nz;
     std::cout << "construtor iniciado\n";
+
+    // criando agora nossa matriz 3d
+    v = new Voxel**[nx];
+    for(int i=0; i< nx; i++){
+        v[i] = new Voxel*[ny];
+
+        for(int j=0; j < ny; j++){
+            v[i][j] = new Voxel[nz];
+
+        }
+}
 }
 
 Sculptor::~Sculptor(){
     std::cout << "destrutor chamado\n";
+    for(int i =0; i< nx; i++){
+        for(int j =0; i< ny; j++){
+            delete v[i][j];
+        }
+    }
+
+    for(int i =0; i< nx; i++){
+        delete v[i];
+    }
+
+    delete v;
+    
+
+
 }
 
+void setColor(float r, float g, float b, float alpha){
+    r = r;
+    g = g;
+    b = b;
+    alpha = alpha;
+}
